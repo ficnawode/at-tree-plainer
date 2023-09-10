6fb0f99cd4fc8fa1fe1e943083e9125ba575fe60
Tobiasz Fic <T.Fic@gsi.de>
2023-09-10 14:08:54 +0200

    Untrack config files

--- scripts/submit_job.sh
+++ scripts/submit_job.sh
@@ -1,27 +1,24 @@
 #!/bin/bash
 
 CONFIG_PATH=$1
-OUTPUT_DIR=$2
 
 timestamp=$(date +"%Y%m%d_%H%M%S")
 NEW_DIR_NAME="plain_${timestamp}"
-RESULT_DIR=/lustre/cbm/users/$USER/pid_plain_trees/logs/$NEW_DIR_NAME
-mkdir -p $RESULT_DIR
-echo "created directory $RESULT_DIR"  
+OUTPUT_DIR=/lustre/cbm/users/$USER/pid_plain_trees/logs/$NEW_DIR_NAME
+mkdir -p $OUTPUT_DIR
+echo "created directory $OUTPUT_DIR"  
 
-LOG_DIR=$RESULT_DIR/log
+LOG_DIR=$OUTPUT_DIR/log
 mkdir -p $LOG_DIR
 mkdir -p $LOG_DIR/out
 mkdir -p $LOG_DIR/error
+echo "intermediate tree will be in output directory: $OUTPUT_DIR"
 echo "logs can be found at $LOG_DIR"
 
 
-if [ $# -ne 2 ]; then
+if [ $# -ne 1 ]; then
   echo "invalid number of parameters, overriding:"
   CONFIG_PATH=$PWD/configs/tof-vtx_1M.json
-  echo "config path: $CONFIG_PATH"
-  OUTPUT_DIR=/lustre/cbm/users/tfic/pid_plain_trees/trees/train
-  echo "output directory: $OUTPUT_DIR"
 fi
 
 sbatch --job-name="at_plain"\
